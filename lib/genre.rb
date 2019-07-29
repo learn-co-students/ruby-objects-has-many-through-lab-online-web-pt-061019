32b4c133dc27dfbaa6fdef7d74dd07551b6f69c3
Valena <valenadelvalle4@gmail.com>
2019-07-29 14:22:45 -0400

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,17 @@
+def initialize(name)
+    @name = name
+    @songs = []
+end
+
+ def add_song(song)
+    @songs << song
+end
+
+ def songs
+    @songs
+end
+
+ def artists
+    @songs.collect { |song| song.artist }
+end
+end
