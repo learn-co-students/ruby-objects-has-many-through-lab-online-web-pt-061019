f27cd162d24769fa17101421f962b97428d29c27
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-08-02 02:16:12 +0000

    Done.

--- spec/04_doctor_spec.rb
+++ spec/04_doctor_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require 'pry'
+
 describe 'Doctor' do
   describe '#name' do
     it 'has a name' do
@@ -16,20 +16,6 @@ describe 'Doctor' do
     end
   end
 
-  describe '#appointments' do
-    it 'returns all appointments associated with this Doctor' do
-      doctor_who = Doctor.new('The Doctor')
-      doctor_smith = Doctor.new('Matt Smith')
-      hevydevy = Patient.new('Devin Townsend')
-      appointment = Appointment.new('Friday, January 32nd', hevydevy, doctor_who)
-      appointment_2 = Appointment.new('Saturday, January 33rd', hevydevy, doctor_smith)
-      
-      expect(doctor_who.appointments).to include(appointment)
-      expect(doctor_smith.appointments).to include(appointment_2)
-
-    end
-  end
-
   describe '#new_appointment' do
     it 'given a date and a patient, creates a new appointment' do
       doctor_who = Doctor.new('The Doctor')
@@ -37,6 +23,17 @@ describe 'Doctor' do
       appointment = doctor_who.new_appointment(hevydevy, 'Friday, January 32nd')
       expect(doctor_who.appointments).to include(appointment)
       expect(appointment.doctor).to eq(doctor_who)
+    end
+  end
+
+  describe '#appointments' do
+    it 'has many appointments' do
+      doctor_who = Doctor.new('The Doctor')
+      hevydevy = Patient.new('Devin Townsend')
+      doctor_who.new_appointment(hevydevy, 'Friday, January 32nd')
+      appointment = doctor_who.new_appointment(hevydevy, 'Saturday, January 33rd')
+      doctor_who.new_appointment(hevydevy, 'Rootenskadootenday, January 315th')
+      expect(doctor_who.appointments).to include(appointment)
     end
   end
 
